4714063f5427c5484106ff7c4ed8f01569df5295
TaxiNumber1729 <lumptran@gmail.com>
2019-03-25 18:30:56 -0500

    Solution to Problem 1.3

---
+++ Problem13.py
@@ -0,0 +1,17 @@
+#Magic Squares and Cards
+import math
+#kings = 13
+#queens = 12
+#Jacks = 11
+
+""" 
+
+    13|11|12 36
+    11|12|13 36 
+    12|13|11 36 
+
+    36 36 36 36
+
+"""
+
+print ("36") 
