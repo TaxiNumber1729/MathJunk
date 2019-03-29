688a7eeee8626a47149763e4fb5f0bd809fc1e4d
TaxiNumber1729 <lumptran@gmail.com>
2019-03-29 17:30:52 -0500

    Solution to Problem 1.4

---
+++ Problem14.py
@@ -0,0 +1,9 @@
+"""
+Two-Cube Calendar
+"""
+
+#Each cube must bear a 0, 1, and 2. Leaving only six faces for the remaining 7 digits.
+
+#Hidden Face (right cube): 0, 1, and 2. (Picture shows 3,4,5 on right cube)
+
+#Hidden Face (left cube): 0, 6 or 9, 7, and 8 (Picture shows 1 and 2 on left cube)
